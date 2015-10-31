df8b43e066d648de24e382edcf86894eaba477e9
Brandon Mott <brandonmotticus@gmail.com>
2015-10-31 02:07:27 -0600

    Added flex css to site

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,56 +1,54 @@
 package controllers
 
-import play.api._
 import models._
 import play.api.data.validation.Constraints._
+import com.typesafe.scalalogging.StrictLogging
 import scala.language.postfixOps
-import play.twirl.api.Html
 import play.api.Play.current
+import play.api._
 import play.api.mvc._
-import play.api.data._, format.Formats._, Forms._
 import play.api.db.DB
+import play.api.libs.json
+import play.api.libs.concurrent.Akka
+import org.joda.time.{LocalDate, DateTime, DateTimeZone}
+import play.api.data.Form
+import play.api.data.Forms._
 import play.api.libs.concurrent.Execution.Implicits._
-import data._
+import play.api.data._
+import play.api.data.validation._
+import scala.concurrent.Future
+import play.twirl.api.Html
+import org.apache.commons.codec.binary.Hex
 
+object Application extends Controller with StrictLogging {
 
-object Application extends Controller {
-
-  val signUpForm = Form(
+  val userForm = Form(
     tuple(
+      "firstName" -> text,
+      "lastName" -> text,
+      "username" -> nonEmptyText(minLength = 6),
       "email" -> email,
       "password" -> text(minLength = 8),
       "password2" -> nonEmptyText
-    ) verifying("Passwords must match", fields => fields match {
-      case (_, pass1, pass2) => pass1 == pass2
-    })
+    ).verifying("Passwords must match", fields => fields match { case (_, _, _, _, pass1, pass2) => pass1 == pass2 })
   )
   
-  case class AccountData(
+  case class UserData(
     firstName: String,
     lastName: String,
+    userName: String,
     email: String,
-    street: String,
-    city: String,
-    state: String,
-    zip: String,
-    phone: String
+    password: String,
+    password2: String
   )  
   
-  val accountForm = Form(
-    mapping(
-      "firstName" -> nonEmptyText,
-      "lastName" -> nonEmptyText,
-      "email" -> email,
-      "street" -> text,
-      "city" -> text,
-      "state" -> text,
-      "zip" -> text,
-      "phone" -> text
-    )(AccountData.apply)(AccountData.unapply)
-  )
+  
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
-
+  
+  def signUp = Action {
+    Ok(views.html.signUp(userForm.fill("","","","","","")))
+  }
 }
